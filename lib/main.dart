e76967b2482c85839ddeb095ba0ab4ece37f8173
Syed Gohar <151599710+SyedGoharHussain@users.noreply.github.com>
2025-08-24 01:00:58 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:greenkeyper/routes.dart';
 import 'package:greenkeyper/theme/app_theme.dart';
+import 'package:greenkeyper/providers/vehicle_provider.dart';
+import 'package:greenkeyper/providers/checklist_provider.dart';
+import 'package:greenkeyper/providers/submission_provider.dart';
+import 'package:greenkeyper/providers/auth_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +16,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      initialRoute: Routes.login,
-      onGenerateRoute: Routes.generateRoute,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => VehicleProvider()),
+        ChangeNotifierProvider(create: (_) => ChecklistProvider()),
+        ChangeNotifierProvider(create: (_) => SubmissionProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        initialRoute: Routes.login,
+        onGenerateRoute: Routes.generateRoute,
+      ),
     );
   }
 }
